d3be8f6cf47b88600c1305aabfd2a20783c59df3
Oleg Borodatov <borolgs@gmail.com>
2023-12-30 23:06:52 +0300

    feat: add http server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,74 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+pub use anyhow::Result;
+use embedded_svc::http::Method;
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    hal::peripherals::Peripherals,
+    http::server::{Configuration, EspHttpServer},
+};
+use std::{thread::sleep, time::Duration};
+use wifi::wifi;
 
-    // Bind the log crate to the ESP Logging facilities
+mod wifi;
+
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_psk: &'static str,
+}
+
+fn main() -> Result<()> {
+    esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    let peripherals = Peripherals::take().unwrap();
+    let sysloop = EspSystemEventLoop::take()?;
+
+    let app_config = CONFIG;
+
+    let _wifi = wifi(
+        app_config.wifi_ssid,
+        app_config.wifi_psk,
+        peripherals.modem,
+        sysloop,
+    )?;
+
+    let mut server = EspHttpServer::new(&Configuration::default())?;
+
+    server.fn_handler("/", Method::Get, |request| {
+        let html = index_html();
+        let mut response = request.into_ok_response()?;
+        response.write(html.as_bytes())?;
+
+        Ok(())
+    })?;
+
+    log::info!("Server awaiting connection");
+
+    loop {
+        sleep(Duration::from_millis(1000));
+    }
+}
+
+fn templated(content: impl AsRef<str>) -> String {
+    format!(
+        r#"
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>esp-rs web server</title>
+    </head>
+    <body>
+        {}
+    </body>
+</html>
+"#,
+        content.as_ref()
+    )
+}
+
+fn index_html() -> String {
+    templated("Hello world!")
 }
